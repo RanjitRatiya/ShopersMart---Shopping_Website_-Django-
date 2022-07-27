4911fab125a8aeb00049a66d1060aa4295fed4c8
Ratiya Ranjit Bhurabhai <64835151+RanjitRatiya@users.noreply.github.com>
2022-07-27 18:10:43 +0530

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = '^_g%33qd(g8bjc+*40&uh(ptgkb$&-*+0!i3$lu7xj1u166cbb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://shopersmart-django-website.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','shopersmart-django-website.herokuapp.com/']
 
 
 # Application definition
